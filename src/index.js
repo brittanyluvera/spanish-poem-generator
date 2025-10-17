e2cb43d92b7ebd91b97de095157951109c107937
Brittany <brittanyluvera@gmail.com>
2025-10-17 09:21:11 -0400

    Added the AI integration

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
-function generatePoem(event) {
-  event.preventDefault();
-
+function displayPoem(response) {
   new Typewriter("#poem", {
-    strings: "Desde mi pequeña vida",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "2344a1ef6tcaba987900e40cb31of63a";
+  let prompt = `User instructions: Generate a Spanish poem about ${instructionsInput.value}`;
+  let context =
+    "You are a romantic poem expert and love to write short poems. Your mission is to generate a 4 line poem in basic html. Don't tell me that you are writing in html. Make sure to follow the user instructions. Only include the poem. Sign written by 'by Romantic AI 🥀' inside <strong> <em> element at the end.";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
