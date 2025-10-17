df3c1fafb419c141fd15575730a601c994b0a5ef
Brittany <brittanyluvera@gmail.com>
2025-10-17 10:11:53 -0400

    Improved the user experience

--- src/index.js
+++ src/index.js
@@ -17,6 +17,10 @@ function generatePoem(event) {
     "You are a romantic poem expert and love to write short poems. Your mission is to generate a 4 line poem in basic html. Don't tell me that you are writing in html. Make sure to follow the user instructions. Only include the poem. Sign written by 'by Romantic AI 🥀' inside <strong> <em> element at the end.";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class= "blink">⌛ Generating a Spanish poem about ${instructionsInput.value}...</div>`;
+
   axios.get(apiUrl).then(displayPoem);
 }
 
